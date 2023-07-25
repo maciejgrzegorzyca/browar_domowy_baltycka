5b0adad047ee357bde6a9c149f41a89a8c4fc8a1
Maciej Grzegorzyca <maciej.grzegorzyca@users.notreply.github.com>
2023-07-25 14:39:21 +0200

    add(models.py):create firts model and migrate it

--- batches/models.py
+++ batches/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Batch(models.Model):
+    namebatch = models.CharField(max_length=26, null=False)
+    stylebatch = models.CharField(max_length=26, null=False)
+    ogbatch = models.FloatField(max_length=4, null=False)
+    abvbatch = models.FloatField(max_length=4, null=False)
+    ibubatch = models.FloatField(max_length=4, null=False)
