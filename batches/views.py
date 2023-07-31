f2406328c3b3a36dcd2177ed8fcaba7f0ddd97e5
Maciej Grzegorzyca <maciej.grzegorzyca@users.notreply.github.com>
2023-07-31 13:24:06 +0200

    feat(bathes.html, views.py)added list of data display

--- batches/views.py
+++ batches/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Batch
 
 def batches(request):
-    template = loader.get_template("batches.html")
-    return HttpResponse(template.render())
+    mybatches = Batch.objects.all().values()
+    template = loader.get_template('batches.html')
+
+    context = {
+        'mybatches': mybatches
+    }
+    return HttpResponse(template.render(context, request))
+
+
+    # template = loader.get_template("batches.html")
+    # return HttpResponse(template.render())
 
 # Create your views here.
