8ef1d8ce25fec43a54d84b2e2689e3f107470d42
Maciej Grzegorzyca <maciej.grzegorzyca@users.notreply.github.com>
2023-11-22 15:11:14 +0100

    change settings and redefined urls

--- browar_domowy_baltycka/settings.py
+++ browar_domowy_baltycka/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "batches",
+    # Add new applications
+    "batches.apps.BatchesConfig",
 ]
 
 MIDDLEWARE = [
