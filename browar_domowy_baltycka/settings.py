7bcbdb3ac945802b17e2676c81194e4fa7500d4f
Maciej Grzegorzyca <maciej.grzegorzyca@users.notreply.github.com>
2024-03-05 11:23:21 +0100

    create app blog

--- browar_domowy_baltycka/settings.py
+++ browar_domowy_baltycka/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # Add new applications
     "batches.apps.BatchesConfig",
-    "pages.apps.PagesConfig"
+    "pages.apps.PagesConfig",
+    "blog.apps.BlogConfig",
 ]
 
 MIDDLEWARE = [
@@ -124,6 +125,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Images:
 
 MEDIA_ROOT = BASE_DIR
 MEDIA_URL = "images/"
--- browar_domowy_baltycka/urls.py
+++ browar_domowy_baltycka/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('pages/', include('pages.urls')),
     path('batches/', include('batches.urls')),
+    path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
