52217b71b55357beb389a69c9d39a0885b95905d
Maciej Grzegorzyca <maciej.grzegorzyca@users.notreply.github.com>
2024-03-05 12:48:22 +0100

    added models to blog and register in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Category, Blog_add
+
 
 # Register your models here.
+#class CategoryAdmin(admin.ModelAdmin):
+ #   list_display = ('category_name',)
+
+
+#admin.site.register(Category, CategoryAdmin)
+
+
+#class BlogAdmin(admin.ModelAdmin):
+#    list_display = ('blog_add_category', 'blog_title', 'blog_description')
+
+#admin.site.register(Blog_add, BlogAdmin)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_name',)
+
+
+
+@admin.register(Blog_add)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('blog_add_category', 'blog_title', 'blog_description')
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=20, unique=True, null=False)
+
+
+
+class Blog_add(models.Model):
+    blog_add_category = models.ForeignKey("blog.Category", on_delete=models.CASCADE, default=None, null=False)
+    blog_title = models.CharField(max_length=26, null=True)
+    blog_description = models.CharField(max_length=26, blank=True)
