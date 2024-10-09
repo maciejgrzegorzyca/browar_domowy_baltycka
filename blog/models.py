03f96ce4a5342bd66f0c958fd755042327679457
Maciej Grzegorzyca <maciejgrzegorzyca@users.noreply.github.com>
2024-10-09 13:46:07 +0200

    added some css

--- blog/models.py
+++ blog/models.py
@@ -1,19 +1,4 @@
 from django.db import models
 
 # Create your models here.
-class Category(models.Model):
-    # Fields
-    category_name = models.CharField(max_length=20, unique=True, null=False)
-
-    # Methods
-    def __str__(self):
-        return self.category_name
-    
-
-class Blog_add(models.Model):
-    # Fields
-    blog_add_category = models.ForeignKey(Category, on_delete=models.CASCADE, default=None, null=False)
-    blog_title = models.CharField(max_length=26, null=True)
-    blog_description = models.TextField(blank=True)
-    image = models.FileField(upload_to="static/images/blog/", blank=True)
 
